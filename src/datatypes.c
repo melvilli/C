f3549f4143a12fc487028cb9b11832f21be12d49
Leviathan melvilli <skmalladi30@gmail.com>
2021-01-02 14:50:08 +0530

    regular sync, still only thinking about cron job

---
+++ src/datatypes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// Declaration and assignment of different types of variables
+	int 	integerVar 	= 	100;
+	float 	floatingVar 	= 	331.79;
+	double  doubleVar 	= 	8.44e+11;
+	char 	charVar 	= 	'W';
+
+	_Bool	boolVar		=	0;
+
+	// Displaying results
+	printf("integerVar   = %i\n", integerVar);
+	printf ("floatingVar = %f\n", floatingVar);
+	printf ("doubleVar   = %e\n", doubleVar);
+	printf ("doubleVar   = %g\n", doubleVar);
+	printf ("charVar     = %c\n", charVar);
+
+	printf ("boolVar     = %i\n", boolVar);
+
+	return 0;
+}
