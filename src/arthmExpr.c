7ae6ac072cb167b2ec298682f7e5e485565e0ab1
Leviathan melvilli <skmalladi30@gmail.com>
2021-01-14 20:03:34 +0530

    Some operators

---
+++ src/arthmExpr.c
@@ -0,0 +1,28 @@
+// Illustrate the use of following expressions
+
+#include <stdio.h>
+
+int main (void)
+{
+	int a = 100;
+	int b = 2;
+	int c = 25;
+	int d = 4;
+	int result;
+
+	result = a - b; //Subtraction
+	printf ("a - b = %i\n", result);
+
+	result = b * c; //Multiplication
+	printf ("a * b = %i\n", result);
+
+	result = a / c; //Division
+	printf ("a / c = %i\n", result);
+
+	result = a + b * c; //Precedence
+	printf ("a + b * c = %i\n", result);
+
+	printf ("a * b + c * d = %i\n", a * b + c * d);
+
+	return 0;
+}
