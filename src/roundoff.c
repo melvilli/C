df013f6b972be1f7d674c0ea7090288b03b83da1
Leviathan melvilli <skmalladi30@gmail.com>
2021-01-19 22:36:20 +0530

    rounding off and intro to for statement

---
+++ src/roundoff.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int i, j, Next_multiple;
+	i = 256;
+	j = 7;
+	Next_multiple = i + j - i % j;
+
+	printf("Closest and largest multiple of 256, divisible by 7 is %i\n", Next_multiple);
+
+	return 0;
+}
